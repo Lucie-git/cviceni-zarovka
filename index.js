3b38426fc73f4ebf3835f249b527e8cb7fcfbd96
Lucie Vadkerti <63465954+Lucie-git@users.noreply.github.com>
2025-04-11 17:55:39 +0200

    solved

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('funguju!');
+//console.log('funguju!');
+
+// Vylepšete program tak, aby na následné stisknutí libovolné klávesy žárovka opět zhasnula (tj. odstraní se třída bulb--on).
+// Opakovaným mačkáním kláves tak můžeme žárovku rozsvěcovat a zhasínat.
+// Když držíte klávesu, prohlížeč automaticky opakovaně posílá událost keydown. Vyzkoušejte, že když podržíte nějakou klávesu, žárovka se bude rychle rozsvěcet a zhasínat.
+
+const light = () => {
+  const bulb = document.querySelector('.bulb');
+  bulb.classList.toggle('bulb--on');
+};
+
+document.addEventListener('keydown', light);
